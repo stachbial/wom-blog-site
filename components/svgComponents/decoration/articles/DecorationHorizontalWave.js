1df56107d224f5f30e041d91789af0faf6bf747f
stachbial <stasiu.bialecki@gmail.com>
2022-05-09 21:04:48 +0200

    enhanced svg icons

--- components/svgComponents/decoration/articles/DecorationHorizontalWave.js
+++ components/svgComponents/decoration/articles/DecorationHorizontalWave.js
@@ -11,8 +11,6 @@ const DecorationHorizontalWave = ({ className, color }) => {
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
         d="M0.5 21.4999V0.499939L772.5 0.499939V21.4999C676 56.1411 531.251 38.8206 386.501 21.5001L386.5 21.4999L386.499 21.4998C241.749 4.17935 96.9998 -13.1411 0.5 21.4999Z"
         fill={color}
       />
